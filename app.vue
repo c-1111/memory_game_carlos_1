d6640ea6b3ecd0ea90fceddf04a2b7d3b8de5acb
Francis Needham <francis@display.emaile>
2025-05-09 17:01:15 +0200

    fix shuffle logic

--- app.vue
+++ app.vue
@@ -1,207 +1,94 @@
 <script setup>
-import { ref, reactive, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { gsap } from 'gsap'
 import { Draggable } from 'gsap/Draggable'
-import Toolbar from './components/toolbar.vue'
-
-// Draggable gsap plugin
 
 gsap.registerPlugin(Draggable)
 
-const startingPoint = ref(null);
+// Shuffle function, pure and returning the shuffled array
+function shuffleArray(array) {
+    const newArray = [...array]
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+            ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+    }
+    return newArray
+}
 
-onMounted(() => {
-    Draggable.create("#startingPoint", {
-        type: "x, y",
-        dragClickables: true
-    });
-    Draggable.create("#yourID", {
-        type: "x, y",
-        dragClickables: true
-    });
-
-    Draggable.create("#draggable_card", {
-        type: "x, y",
-        dragClickables: true,
-    });
+// Fetch data and shuffle server-side
+const { data: cards } = await useAsyncData('cards', () => {
+    const baseData = [
+        { handle: "aaa", interior: "https://picsum.photos/id/237/200/300", locked: false, flipped: false },
+        { handle: "bbb", interior: "https://picsum.photos/id/10/200/300", locked: false, flipped: false },
+        { handle: "ccc", interior: "https://picsum.photos/id/1/200/300", locked: false, flipped: false },
+        { handle: "ddd", interior: "https://picsum.photos/id/2/200/300", locked: false, flipped: false },
+        { handle: "eee", interior: "https://picsum.photos/id/3/200/300", locked: false, flipped: false },
+        { handle: "fff", interior: "https://picsum.photos/id/4/200/300", locked: false, flipped: false },
+        { handle: "ggg", interior: "https://picsum.photos/id/5/200/300", locked: false, flipped: false },
+        { handle: "hhh", interior: "https://picsum.photos/id/6/200/300", locked: false, flipped: false },
+        { handle: "iii", interior: "https://picsum.photos/id/7/200/300", locked: false, flipped: false },
+    ]
+    const duplicated = baseData.concat(structuredClone(baseData))
+    return shuffleArray(duplicated)
 })
 
-//-------------------------------------------------------------------------------------------------------------------------------
-// ANIMATION OKEY
+// DOM refs
+const startingPoint = ref(null)
 
-const initGame = () => {
-    resetGame();
-    // GETTING THE POSITION OF THE ORIGIN ELEMENT - IF WE WRAP THIS INTO A FUNCTION WE CAN TRIGGER IT AFTER DRAG ETC.
-    const position = startingPoint.value.getBoundingClientRect();
-    // THIS FUNCTION EVERY TIME IT'S CALLED GETS THE ELEMENT COORDINATES
+// Animation setup
+onMounted(() => {
+    Draggable.create("#startingPoint", { type: "x,y", dragClickables: true })
+    Draggable.create("#yourID", { type: "x,y", dragClickables: true })
+    Draggable.create("#draggable_card", { type: "x,y", dragClickables: true })
+    initGame()
+})
+
+function initGame() {
+    resetGame()
+    const position = startingPoint.value.getBoundingClientRect()
     const x_center = window.innerWidth / 2
     const y_center = window.innerHeight / 2
-    // HERE I'M GETTING THE CENTER OF THE PAGES IN PX. PROBLEM HERE IS THAT IF YOU RESIZE THE WINDOW THE COORDINATES NEEDS TO BE UPDATED
     const x_origin = position.x - x_center
     const y_origin = position.y
 
+    gsap.set('.card_wrapper', { x: x_origin, y: y_origin, scale: 0.5, opacity: 0 })
+    gsap.to('.card_wrapper', { x: 0, y: 0, opacity: 1, stagger: 0.2, scale: 1, duration: 1, ease: 'power2.out' })
+}
 
-
-    gsap.set('.card_wrapper', {
-        x: x_origin,
-        y: y_origin,
-        scale: .5,
-        opacity: 0
-    })
-    gsap.to('.card_wrapper', {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        stagger: 0.2,
-        scale: 1,
-        duration: 1,
-        ease: 'power2.out'
+// Logic functions
+function resetGame() {
+    cards.value.forEach(el => {
+        el.locked = false
+        el.flipped = false
     })
 }
 
-onMounted(() => {
-    initGame();
-})
-//-------------------------------------------------------------------------------------------------------------------------------
-
-
-// DATA LAYER WITH ONLY THE DATA I ACTUALLY REQUIRE ( no doubles, no shuffle )
-// THINK TOP DOWN 
-
-const data = [{
-    handle: "aaa",
-    interior: "https://picsum.photos/id/237/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "bbb",
-    interior: "https://picsum.photos/id/123/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "ccc",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "ddd",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "eee",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "fff",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "ggg",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "hhh",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-{
-    handle: "iii",
-    interior: "https://picsum.photos/id/1/200/300",
-    locked: false,
-    flipped: false
-},
-];
-
-// DIFF BETWEEN CLONE ( copies as reference ) AND DEEPCLONE ( creates new objects also if duplicated )
-const data_copy = structuredClone(data);
-// make an unique array 
-const card_array = data.concat(data_copy);
-// HERE WE SHOULD SHUFFLE THE ARRAY
-
-const shuffleArray = (data_copy) => {
-    for (let i = card_array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [card_array[i], card_array[j]] = [card_array[j], card_array[i]];
-    }
-}
-shuffleArray(card_array)
-
-
-//-------------------------------------------------------------------------------------------------------------------------------
-// // SHUFFLE --> I am not understanding how this shuffles index and fucks up the game logic not touching the handle 
-// const shuffleArray = (data_copy) => {
-//   for (let i = card_array.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [card_array[i], card_array[j]] = [card_array[j], card_array[i]];
-//   }
-//   return data_copy;
-// };
-
-// shuffleArray() => missing the parameter , shufflearray is a function and needs to know which array to shuffle 
-//-------------------------------------------------------------------------------------------------------------------------------
-
-
-const cards = ref(card_array)
-
-
-// LOGIC HERE IS THAT I PASS THE ARRAY OF THE TWO FILPPED CARDS. I CHECK IF THEY ARE MATCHING. 
-// THEN BY LOPPING THE ARRRAY I FOUND THE REF IN THE ORIGINAL ARRAY AND CHANGE THE DATA BASED ON THE MATCH
-// AT THE END I LAUNCH THE CHECKRESUT FUNCTION TO HANDLE THE STATE OF THE GAME
-const parseCards = function (flipped_cards) {
-    const result = (flipped_cards[0].handle == flipped_cards[1].handle)
-    flipped_cards.forEach((el) => {
-        cards.value[el.index].locked = result ? true : false
-        cards.value[el.index].flipped = result ? true : false
+function parseCards(flipped_cards) {
+    const result = flipped_cards[0].handle === flipped_cards[1].handle
+    flipped_cards.forEach(el => {
+        cards.value[el.index].locked = result
+        cards.value[el.index].flipped = result
     })
-    checkResults();
+    checkResults()
 }
-// I COMPARE THE LENGTH OF A FILTERED ARRAY BASED ON THE LOCKED FIELD. 
-// IF THE LENGTH IS THE SAME AS THE CARD ARRAY IT'S A WIN
-const checkResults = function () {
-    const locked_cards = cards.value.filter((el) => el.locked)
-    if (locked_cards.length == cards.value.length) {
+
+function checkResults() {
+    const locked_cards = cards.value.filter(el => el.locked)
+    if (locked_cards.length === cards.value.length) {
         alert("BAZINGA")
         resetGame()
     }
 }
 
-// LOOP TROUGHT THE ORIGINAL ARRAY AND RESET THE STATUS // not needed anymore ??
-const resetGame = function () {
-    cards.value.forEach((el) => {
-        el.locked = false;
-        el.flipped = false;
-    })
-}
-
-// WE ARE WORKING WITH CLASS BASED ANIMATION - MAYBE GSAP TO BE SPECTACULAR OR EASIER TO HANDLE ( TIMING ETC.)
-const flipCard = function (index) {
+function flipCard(index) {
     cards.value[index].flipped = !cards.value[index].flipped
-    // THIS NEXT LINE MAKES THE MAGIC- BY ASSIGNING THE INDEX OF THE CARD TO THE OBJECT IN THE ARRAY I HAVE AN UNIQ REF.
     cards.value[index].index = index
-    const flipped_cards = cards.value.filter((el) => el.flipped && !el.locked)
-    if (flipped_cards.length == 2) {
-        setTimeout(() => {
-            parseCards(flipped_cards)
-        }, "1000");
-
+    const flipped_cards = cards.value.filter(el => el.flipped && !el.locked)
+    if (flipped_cards.length === 2) {
+        setTimeout(() => parseCards(flipped_cards), 1000)
     }
-
-
 }
 </script>
-
 
 <template>
     <div class="app">
@@ -209,6 +96,7 @@ const flipCard = function (index) {
             <img src="/public/img/zip.png" alt="">
             <p>MEMORY</p>
         </div>
+
         <div class="cards_container">
             <div v-for="(card, index) in cards" :key="index" class="card_wrapper" id="draggable_card"
                 @click="flipCard(index)">
@@ -239,7 +127,6 @@ const flipCard = function (index) {
     img {
         height: 50px;
         margin-bottom: .3rem
-
     }
 
     p {
