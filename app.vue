48f0ac7424cae9621519c85f197756683aad7473
c-1111 <cmartinez4086@esdmadrid.es>
2025-05-12 09:57:41 +0200

    cards_component_created

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue'
 import { gsap } from 'gsap'
 import { Draggable } from 'gsap/Draggable'
+import Cards from './components/cards.vue'
 
 gsap.registerPlugin(Draggable)
 
@@ -109,20 +110,8 @@ function flipCard(index) {
             <img src="/public/img/zip.png" alt="">
             <p>MEMORY</p>
         </div>
-
-        <div class="cards_container">
-            <div v-for="(card, index) in cards" :key="index" class="card_wrapper" id="draggable_card"
-                @click="flipCard(index)">
-                <div class="card cards_container_card" :class="{ flipped: card.flipped, locked: card.locked }">
-                    <div class="card_face card_back">
-                        <img src="/public/img/card.png" alt="back" />
-                    </div>
-                    <div class="card_face card_front">
-                        <img :src="card.interior" alt="interior" />
-                    </div>
-                </div>
-            </div>
-        </div>
+        
+        <Cards :cards="cards" @flip="flipCard" />
         <OtherApps />
         <Toolbar :resetGame="resetGame" :initGame="initGame" />
     </div>
@@ -160,53 +149,5 @@ function flipCard(index) {
     align-items: center
     justify-content: center
     padding-bottom: 10rem
-    .cards_container 
-        margin: auto
-        display: grid
-        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 
-        gap: 2rem
-        .card_wrapper
-            width: 100px
-            height: 150px 
-            text-align: center
-            flex: 0 0 100px
-            padding: 4px
-            position: relative
-            cursor: pointer
-            .cards_container_card 
-                transform-style: preserve-3d 
-                transition: transform ease 0.6s 
-                &.flipped 
-                    transform: rotateY(180deg) scale(1.2)
-                &.locked 
-                    opacity: 0
-                    pointer-events: none
-            .card 
-                width: 100%
-                height: 100%
-                position: relative
-                transform-style: preserve-3d
-                .card_face 
-                    position: absolute
-                    width: 100%
-                    height: 100%
-                    backface-visibility: hidden 
-                    display: flex
-                    align-items: center
-                    justify-content: center                  
-                .card_back 
-                    img
-                        width: 100%
-                        height: 100%
-                        object-fit: cover
-                .card_front 
-                    //background: white
-                    transform: rotateY(180deg) 
-                    img
-                        //width: 100%
-                        //height: 100%
-                        width: 100%
-                        height: auto
-                        display: block
-                        object-fit: contain
+    
 </style>
--- components/cards.vue
+++ components/cards.vue
@@ -1,12 +1,76 @@
 <script setup>
-// const props = defineProps({
-//     resetGame: {
-//         type: Function,
-//         required: true
-//     },
-//     initGame: {
-//         type: Function,
-//         required: true
-//     }
-// });
-// </script>
+defineProps({
+    cards: Array
+})
+
+const emit = defineEmits(['flip'])
+
+function flipCard(index) {
+    emit('flip', index)
+}</script>
+<template>
+    <div class="cards_container">
+        <div v-for="(card, index) in cards" :key="index" class="card_wrapper" id="draggable_card"
+            @click="flipCard(index)">
+            <div class="card cards_container_card" :class="{ flipped: card.flipped, locked: card.locked }">
+                <div class="card_face card_back">
+                    <img src="/public/img/card.png" alt="back" />
+                </div>
+                <div class="card_face card_front">
+                    <img :src="card.interior" alt="interior" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.cards_container 
+    margin: auto
+    display: grid
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 
+    gap: 2rem
+    .card_wrapper
+        width: 100px
+        height: 150px 
+        text-align: center
+        flex: 0 0 100px
+        padding: 4px
+        position: relative
+        cursor: pointer
+        .cards_container_card 
+            transform-style: preserve-3d 
+            transition: transform ease 0.6s 
+            &.flipped 
+                transform: rotateY(180deg) scale(1.2)
+            &.locked 
+                opacity: 0
+                pointer-events: none
+        .card 
+            width: 100%
+            height: 100%
+            position: relative
+            transform-style: preserve-3d
+            .card_face 
+                position: absolute
+                width: 100%
+                height: 100%
+                backface-visibility: hidden 
+                display: flex
+                align-items: center
+                justify-content: center                  
+            .card_back 
+                img
+                    width: 100%
+                    height: 100%
+                    object-fit: cover
+            .card_front 
+                //background: white
+                transform: rotateY(180deg) 
+                img
+                    //width: 100%
+                    //height: 100%
+                    width: 100%
+                    height: auto
+                    display: block
+                    object-fit: contain</style>
